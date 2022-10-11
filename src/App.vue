07fa3a6dadcc62d96c294e7908c39bdb5fccfc9d
JD Singh <jagdeepsingh@founderandlightning.com>
2022-10-12 01:36:28 +0530

    There you go... go fly...

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div class="h-full flex flex-col justify-between items-center">
+    <div>
+      <div class="mt-10">
+        <a href="/">
+          <img src="/jdecode-dev.svg" class="logo w-48 h-48" alt="jdecode.dev"/>
+        </a>
+      </div>
+    </div>
+    <Footer/>
+  </div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    server: {
+        host: "0.0.0.0",
+        port: 1111,
+    },
+    plugins: [vue()]
+})
