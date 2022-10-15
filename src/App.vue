14aa398945d8452d283421e291b68b192675e077
JD Singh <jagdeepsingh@founderandlightning.com>
2022-10-15 23:12:57 +0530

    Update content

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup>
 import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div class="h-full flex flex-col justify-between items-center">
-    <div>
-      <div class="mt-10">
-        <a href="/">
-          <img src="/jdecode-dev.svg" class="logo w-48 h-48" alt="jdecode.dev"/>
-        </a>
-      </div>
-    </div>
-    <Footer/>
+  <div class="mx-auto flex flex-col justify-start items-center h-full">
+    <Header></Header>
+  </div>
+  <div class="">
+    <Footer></Footer>
   </div>
 </template>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <a href="/" class="hidden">
+    <img src="/jdecode-dev.svg" class="logo w-24 h-24" alt="jdecode.dev" />
+  </a>
+  <div class="pt-10 text-5xl flex justify-center">
+    <div>
+      <span class="text-orange-600 text-7xl">J</span>
+      <span>A</span>
+      <span>G</span>
+    </div>
+    <div>
+      <span class="text-orange-600 text-7xl">D</span>
+      <span>E</span>
+      <span>E</span>
+      <span>P</span>
+    </div>
+  </div>
+  <div class="pt-10 text-7xl flex justify-center">
+    <div>
+      <span class="text-orange-600">SINGH</span>
+    </div>
+  </div>
+  <div class="pt-10 mt-5 text-4xl flex flex-col justify-center text-center gap-5">
+    <p>
+      <span class="text-orange-500">PHP</span> developer since <span class="text-orange-500">2006</span>
+    </p>
+    <p>
+      Creating websites in <span class="text-orange-500">Javascript since 2002</span>
+    </p>
+    <p>
+      <span class="text-orange-500">NOT</span> a full-stack developer
+    </p>
+    <p class="text-2xl mt-5 italic">
+      laravel postgres googlecloud vue tailwind docker github phpstorm terraform heroku
+    </p>
+  </div>
+</template>
+
